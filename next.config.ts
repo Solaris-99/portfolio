6ee6b9befffed9ef20fee653612133a299d1ac9c
Solaris-99 <e.cifuentes996@gmail.com>
2025-05-27 23:57:37 -0300

    config: next config corrections

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
+const repo = "portfolio"
+
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export', // 👈 Required for Next.js 13+ static export
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  trailingSlash: true 
 };
 
 export default nextConfig;
